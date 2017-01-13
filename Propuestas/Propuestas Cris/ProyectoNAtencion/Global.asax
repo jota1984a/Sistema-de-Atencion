90d5174bd17da2b3e5e56d241bfeeed8ea8e50a5
crisgg <maxcorreoimo@gmail.com>
2017-01-13 20:09:28 -0300

    mi proyecto
    .

---
+++ Propuestas/Propuestas Cris/ProyectoNAtencion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoNAtencion.MvcApplication" Language="C#" %>
---
+++ Propuestas/Propuestas Cris/ProyectoNAtencion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoNAtencion.Startup))]
+namespace ProyectoNAtencion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
